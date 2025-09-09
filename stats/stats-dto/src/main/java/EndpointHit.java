148d4430d8a3fd3ed22a694b0cf323adc4f01d29
EugeniyZrol <eu.zrol@yandex.ru>
2025-09-09 19:40:07 +0500

    дто сущности

---
+++ stats/stats-dto/src/main/java/EndpointHit.java
@@ -0,0 +1,31 @@
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EndpointHit {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotBlank(message = "App cannot be blank")
+    private String app;
+
+    @NotBlank(message = "Uri cannot be blank")
+    private String uri;
+
+    @NotBlank(message = "Ip cannot be blank")
+    private String ip;
+
+    @NotNull(message = "Timestamp cannot be null")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
