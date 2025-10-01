132c1f261397a97abd7fd27264102fc8fea64938
EugeniyZrol <eu.zrol@yandex.ru>
2025-10-01 18:47:32 +0500

    чекстайл

--- main-service/src/main/java/ewm/compilation/repository/CompilationRepository.java
+++ main-service/src/main/java/ewm/compilation/repository/CompilationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.lang.NonNull;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Long>, JpaSpecificationExecutor<Compilation> {
@@ -15,8 +14,6 @@ public interface CompilationRepository extends JpaRepository<Compilation, Long>,
     Page<Compilation> findByPinned(Boolean pinned, Pageable pageable);
 
     Optional<Compilation> findById(@NonNull Long id);
-
-    boolean existsByTitle(String title);
 
     boolean existsById(@NonNull Long compId);
 }
